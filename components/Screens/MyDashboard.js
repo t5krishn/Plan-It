3759bb732afca058669a5fc8a32d6f16ab7a7b1f
vagrant <tk95@live.ca>
2019-09-02 18:36:54 +0000

    Added wireframe for main page

--- components/Screens/MyDashboard.js
+++ components/Screens/MyDashboard.js
@@ -1,27 +1,73 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
+import CalendarMonth from "../CalendarMonth";
+import TripsList from  "../TripsList";
 
 export default function Dashboard({ navigation }) {
 	return (
-		<View style={styles.container}>
-			<Text>DashboardScreen (Trips Overview)</Text>
-			<Button
-				title="San Diego Trip"
-				onPress={() => navigation.navigate("TabNavigator")}
-			/>
+		<View style={styles.mainScreenContainer}>
+			<View style={styles.topContainer}>
+				<Text style={styles.titleText}>
+					DashboardScreen (Trips Overview)
+				</Text>
+				
+				<View style={styles.calendarContainer}>
+					<CalendarMonth />
+				</View>
+			</View>
+
+			<View style={styles.bottomContainer}>
+
+				<ScrollView
+				style = {styles.tripsScrollContainer}
+				contentContainerStyle={styles.tripsContent}>
+
+					<TripsList navigation={navigation}/>
+
+				</ScrollView>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
+	mainScreenContainer: {
 		flex: 1,
+		flexDirection: "column",
 		justifyContent: "center",
-		alignItems: "center"
+		alignItems: "center",
+		backgroundColor: "skyblue"
 	},
-	TextInput: {
-		borderColor: "black",
-		borderWidth: 1,
-		width: 100
+	topContainer: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "lightgreen",
+		width: "100%"
+	},
+	bottomContainer: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "lightblue",
+		width: "100%"
+	},
+	titleText: {
+		fontSize: 20,
+		marginTop: "12%"
+	},
+	calendarContainer: {
+		flex: 1,
+		backgroundColor: "lightgrey",
+		marginTop: "2%",
+	},
+	tripsScrollContainer : {
+		width : "100%"
+	},
+	tripsContent: {
+		paddingBottom: 10,
+		// backgroundColor: "yellow"
 	}
 });
