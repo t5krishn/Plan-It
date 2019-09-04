5e0a1dfe0938e09f87371ed31f579f5d038a65cb
vagrant <tk95@live.ca>
2019-09-04 00:48:26 +0000

    Added redux, mapToState and connected component to redux store

--- components/Screens/myDashboard/MyDashboard.js
+++ components/Screens/myDashboard/MyDashboard.js
@@ -1,89 +1,116 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
 import CalendarMonth from "./CalendarMonth";
-import TripsList from  "./TripsList";
+import TripsList from "./TripsList";
 
-export default function Dashboard({ navigation }) {
+import { connect } from "react-redux";
 
-	let [trips, setTrips] = useState([]);
+import { selectTrip, fetchTripData } from "../../../store/actions/tripActions"
 
-	const getAllTrips = () => {
-		fetch("http://localhost:3000/trip")
-		.then(res => res.json())
-		.then(data => {
-			console.log(data);
-			setTrips(data);
-		});
-	}
-	
-	useEffect(()=> {getAllTrips()}, [])
 
-	return (
-		<View style={styles.mainScreenContainer}>
-			<View style={styles.topContainer}>
-				<Text style={styles.titleText}>
-					DashboardScreen (Trips Overview)
-				</Text>
-				
-				<View style={styles.calendarContainer}>
-					<CalendarMonth />
-				</View>
-			</View>
+function Dashboard(props) {
+  let [trips, setTrips] = useState([]);
 
-			<View style={styles.bottomContainer}>
+  const getAllTrips = () => {
+    fetch("http://localhost:5422/trip")
+      .then(res => res.json())
+      .then(data => {
+        setTrips(data);
+      });
+  };
 
-				<ScrollView
-				style = {styles.tripsScrollContainer}
-				contentContainerStyle={styles.tripsContent}>
-					<TripsList
-					navigation={navigation}
-					trips={trips}/>
-				</ScrollView>
-			</View>
-		</View>
-	);
+  const onPressTripHandler = (trip_id) => {
+	props.dispatch(selectTrip(trip_id))
+	props.dispatch(fetchTripData(trip_id))
+	props.navigation.navigate("TabNavigator")
+  }
+
+  useEffect(() => {
+    getAllTrips();
+  }, []);
+
+  return (
+    <View style={styles.mainScreenContainer}>
+      <View style={styles.topContainer}>
+        <Text style={styles.titleText}>DashboardScreen (Trips Overview):</Text>
+
+        <View style={styles.calendarContainer}>
+          <CalendarMonth />
+        </View>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <ScrollView
+          style={styles.tripsScrollContainer}
+          contentContainerStyle={styles.tripsContent}
+        >
+          <TripsList onPress={onPressTripHandler} trips={trips} />
+        </ScrollView>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	mainScreenContainer: {
-		flex: 1,
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "skyblue"
-	},
-	topContainer: {
-		flex: 1,
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "lightgreen",
-		width: "100%"
-	},
-	bottomContainer: {
-		flex: 1,
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "lightblue",
-		width: "100%"
-	},
-	titleText: {
-		fontSize: 20,
-		marginTop: "12%"
-	},
-	calendarContainer: {
-		flex: 1,
-		backgroundColor: "lightgrey",
-		marginTop: "2%",
-	},
-	tripsScrollContainer : {
-		width : "100%"
-	},
-	tripsContent: {
-		paddingBottom: 10,
-		// backgroundColor: "yellow"
-	}
+  mainScreenContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "skyblue"
+  },
+  topContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgreen",
+    width: "100%"
+  },
+  bottomContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightblue",
+    width: "100%"
+  },
+  titleText: {
+    fontSize: 20,
+    marginTop: "12%"
+  },
+  calendarContainer: {
+    flex: 1,
+    backgroundColor: "lightgrey",
+    marginTop: "2%"
+  },
+  tripsScrollContainer: {
+    width: "100%"
+  },
+  tripsContent: {
+    paddingBottom: 10
+    // backgroundColor: "yellow"
+  }
 });
 
+function mapStateToProps(state) {
+  const { selectedTrip, gettingTripData } = state;
+  const { isFetching, events, toDos, expenses } = gettingTripData[
+    selectedTrip
+  ] || {
+    isFetching: true,
+	events: [],
+	toDos: [],
+	expenses: []
+  }
 
+  return {
+	selectedTrip,
+	isFetching,
+	events,
+	toDos,
+	expenses
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
