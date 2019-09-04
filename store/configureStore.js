04e7a968b7d7fde4c3965526db3cd1d0b1ba8233
vagrant <tk95@live.ca>
2019-09-04 00:51:19 +0000

    Configured store for redux using reducers and middleware

---
+++ store/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import tripReducer from './reducers/tripReducer'
+
+const loggerMiddleware = createLogger()
+
+export default function configureStore(preloadedState) {
+  return createStore(
+    tripReducer,
+    preloadedState,
+    applyMiddleware(thunkMiddleware, loggerMiddleware)
+  )
+}
