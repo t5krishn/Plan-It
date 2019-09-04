a1d8180aa087dbf421d5b940b9ca0754dad20af8
vagrant <tk95@live.ca>
2019-09-04 14:37:31 +0000

    Changed path for myexpenses, myfriends and mysettings

--- App.js
+++ App.js
@@ -30,9 +30,9 @@ import Dashboard from "./components/Screens/myDashboard/MyDashboard";
 import EventsTab from "./components/Screens/Tabs/EventsTab";
 import ExpensesTab from "./components/Screens/Tabs/ExpensesTab";
 import TodoTab from "./components/Screens/Tabs/TodoTab";
-import MyExpenses from "./components/Screens/MyExpenses";
-import MyFriends from "./components/Screens/MyFriends.js";
-import MySettings from "./components/Screens/MySettings";
+import MyExpenses from "./components/Screens/Drawers/MyExpenses";
+import MyFriends from "./components/Screens/Drawers/MyFriends.js";
+import MySettings from "./components/Screens/Drawers/MySettings";
 import NewTrip from "./components/Screens/NewTrip";
 
 /*
