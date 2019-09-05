1e2c032cd3b72cc6471505027fa7befcf71948f6
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-09-05 12:28:53 -0400

    added icons for add-btn

--- components/Buttons/Addbtn.js
+++ components/Buttons/Addbtn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, Dimensions } from "react-native";
+import { View, Dimensions } from "react-native";
 import { FloatingAction } from "react-native-floating-action";
 
 export default function AddBtn({ navigation }) {
@@ -25,6 +25,7 @@ export default function AddBtn({ navigation }) {
 const actions = [
 	{
 		text: "Add Event",
+		icon: require("../../assets/calendar.png"),
 		name: "bt_event",
 		position: 1,
 		textBackground: "none",
@@ -32,6 +33,7 @@ const actions = [
 	},
 	{
 		text: "Add To do",
+		icon: require("../../assets/check.png"),
 		name: "bt_todo",
 		position: 2,
 		textBackground: "none",
@@ -39,18 +41,10 @@ const actions = [
 	},
 	{
 		text: "Add Expense",
+		icon: require("../../assets/dollar-symbol.png"),
 		name: "bt_expense",
 		position: 3,
 		textBackground: "none",
 		textColor: "white"
 	}
 ];
-
-const styles = StyleSheet.create({
-	actionButtonIcon: {
-		fontSize: 20,
-		height: 22,
-		color: "white",
-		zIndex: 9
-	}
-});
